7bb74c2b63c72ab854292a3e5e9937dc78b274ee
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-03-02 12:20:58 +0300

    1

--- DR_solution/csv2json.py
+++ DR_solution/csv2json.py
@@ -77,11 +77,10 @@ for filename in os.listdir(inputFolder):
     file = os.path.join(inputFolder, filename)
     print('Reading {}'.format(file))
     csvData = pandas.read_csv(file)
-    newFilename = filename[-20:] + '.json'
+    newFilename = os.path.join(outputFolder, filename[:-4] + '.json')
     print(newFilename)
     for RIC in csvData['RIC']:
       jsonBlueprint['RICs'].append(RIC)
     for field in csvData.columns:
       jsonBlueprint['fields'].append(field)
-
-print(json.dumps(jsonBlueprint, ensure_ascii = True, indent = 2))
+    
